93da507d002e01c582829a6c006653e0f6584e50
PriyaS0309 <109306451+PriyaS0309@users.noreply.github.com>
2022-08-05 18:40:01 +0530

    Stored Procedure done and operators

---
+++ SqlQuery(StoredProcedure).sql
@@ -0,0 +1,86 @@
+﻿create procedure spGetEmpInfo
+as
+begin
+
+select Employee_city,Employee_name,Employee_Id from Employee       --SP Example--
+where Employee_salary>20000;
+
+end;
+
+execute spGetEmpInfo;
+
+
+create procedure spEmployeeId
+@id int								                       --sp with single parameter
+as
+begin                                                         
+
+select * from Employee where Employee_Id=@id 
+
+end;
+
+execute spEmployeeId 114;                      --executing procedure with Id present in employee table that details are needed --
+
+
+
+ create procedure spMyEmployeeDetails
+@id int,
+@name varchar(30)                              --creating procedure with multile parameters---
+
+as
+begin
+
+select * from Employee where Employee_Id=@id and Employee_name=@name
+
+end;
+
+
+exec spMyEmployeeDetails 112,'Shweta';
+
+sp_helptext spMyEmployeeDetails;
+
+
+alter Procedure spMyEmployeeDetails
+@id int,
+@name varchar(30)                                        --altering procedure--
+with encryption
+as
+begin
+
+select Employee_city,Employee_salary,Employee_name from Employee where Employee_Id=@id and Employee_name=@name
+
+end;
+
+exec spMyEmployeeDetails 112,'Shweta';
+
+
+------------------------------------
+
+sp_helptext spMyEmployeeDetails;
+
+---------------------------------------------
+
+alter Procedure spMyEmployeeDetails
+@id int,
+@name varchar(30)                                        --altering procedure and making it encrypted--
+with encryption
+as
+begin
+
+select Employee_city,Employee_salary,Employee_name from Employee where Employee_Id=@id and Employee_name=@name
+
+end
+
+sp_helptext spMyEmployeeDetails;                       --to see if it is encrypted---
+
+
+drop procedure spMyEmployeeDetails;                    -- to delete SP----
+
+sp_helptext spMyEmployeeDetails;
+
+
+
+
+
+
+
---
+++ SqlQuery(operator).sql
@@ -0,0 +1,18 @@
+﻿use demo;
+
+select * from Employee where Employee_salary between 20000 and 30000;
+
+select top 3 * from Employee;
+
+select top 50 percent * from Employee;                            --this query gives the top 50% records of Employee table--
+
+select distinct * from Employee;                ---gives only the unique values--
+
+select * from Employee where Employee_salary in (20000,30000,27000);
+
+select * into EmployeeNew from Employee;
+
+Select * from EmployeeNew;
+
+sp_renamedb 'demo1','demo';
+
